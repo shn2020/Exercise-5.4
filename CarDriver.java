46ff1945e6f373b5fb4b3678cf3cc540f98188d9
shn2020 <shn2020@email.vccs.edu>
2016-04-13 16:01:21 -0400

    Update CarDriver.java

--- CarDriver.java
+++ CarDriver.java
@@ -11,6 +11,21 @@ public class CarDriver {
 		userCar.setEngineSize(Double.parseDouble(JOptionPane.showInputDialog("Set horse power: ")));
 		userCar.setMake(JOptionPane.showInputDialog("Set make: "));
 		
+		hisCar.setColor(JOptionPane.showInputDialog("Set color: "));
+		hisCar.setHorsePower(Integer.parseInt(JOptionPane.showInputDialog("Set horse power: ")));
+		hisCar.setEngineSize(Double.parseDouble(JOptionPane.showInputDialog("Set horse power: ")));
+		hisCar.setMake(JOptionPane.showInputDialog("Set make: "));
+		
+		herCar.setColor(JOptionPane.showInputDialog("Set color: "));
+		herCar.setHorsePower(Integer.parseInt(JOptionPane.showInputDialog("Set horse power: ")));
+		herCar.setEngineSize(Double.parseDouble(JOptionPane.showInputDialog("Set horse power: ")));
+		herCar.setMake(JOptionPane.showInputDialog("Set make: "));
+		
+		myCar.setColor(JOptionPane.showInputDialog("Set color: "));
+		myCar.setHorsePower(Integer.parseInt(JOptionPane.showInputDialog("Set horse power: ")));
+		myCar.setEngineSize(Double.parseDouble(JOptionPane.showInputDialog("Set horse power: ")));
+		myCar.setMake(JOptionPane.showInputDialog("Set make: "));
+		
 		JOptionPane.showMessageDialog(null,userCar.toString());
 		System.out.println("Number of cars: " + Car.count);
 		System.out.println("userCar equal to myCar ? " + userCar.equals(myCar));
